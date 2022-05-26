aa156365ad9247f94decba66dd0dedd20fa950bd
sorikiki98 <sorikiki98@sookmyung.ac.kr>
2022-05-26 11:10:26 +0900

    [ADD] mysql and openapi connection test

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import mysql from 'mysql';
+import yamljs from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
 const app = express();
-console.log("hello");
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Kyesolkim0626!',
+    database: 'debugging',
+    port: 3306,
+});
+pool.getConnection((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Connected..');
+});
+const apiJSDocument = yamljs.load('./api/openapi.yaml');
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiJSDocument));
 app.listen(8080);
 //# sourceMappingURL=app.js.map
